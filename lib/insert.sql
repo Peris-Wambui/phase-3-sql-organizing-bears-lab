6dbac25451e965e29eaff12383882f238e2a0bbf
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-10-27 16:05:31 -0400

    populated the bears table with 7 rows and one with no value

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 1, "male",
+"brown", NULL, "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "male",
+"skyblue", NULL, "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "female",
+"navyblue", NULL, "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "male",
+"orange", NULL, "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "female",
+"grey", NULL, "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "male",
+"yellow", NULL, "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "female",
+"blue", NULL, "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL,NULL, NULL);
